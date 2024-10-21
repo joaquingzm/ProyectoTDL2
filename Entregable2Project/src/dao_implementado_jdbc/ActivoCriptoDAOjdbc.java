d21f399199a9f5980e0ee550c204f39fc9fb0fd3
Joaquín Guzmán <joaquin.gzm234@gmail.com>
2024-10-21 19:07:07 -0300

    Implementado daojdbc de activo cripto

--- Entregable2Project/src/dao_implementado_jdbc/ActivoCriptoDAOjdbc.java
+++ Entregable2Project/src/dao_implementado_jdbc/ActivoCriptoDAOjdbc.java
@@ -2,9 +2,11 @@ package dao_implementado_jdbc;
 
 import java.util.Comparator;
 import java.util.List;
-
+import java.sql.*;
 import dao_interfaces.ActivoCriptoDAO;
 import modelos.ActivoCripto;
+import singletones.MyConnection;
+import singletones.MyStatement;
 
 public class ActivoCriptoDAOjdbc implements ActivoCriptoDAO{
 
@@ -16,7 +18,14 @@ public class ActivoCriptoDAOjdbc implements ActivoCriptoDAO{
 
 	@Override
 	public List<ActivoCripto> listarActivosCripto(Comparator<ActivoCripto> c) {
-		// TODO Auto-generated method stub
+		Statement stmt = MyStatement.getStmt();
+		String sql = " SELECT * FROM ACTIVO_CRIPTO";
+		try {
+			ResultSet resul = stmt.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
