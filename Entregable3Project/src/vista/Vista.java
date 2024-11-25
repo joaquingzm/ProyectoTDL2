adbe92c7179b45540320771579959e5be3758fe3
Octavio Tomas Isabella Valenzi <valenzi.octavio@gmail.com>
2024-11-25 20:33:50 -0300

    haciendo vista

--- Entregable3Project/src/vista/Vista.java
+++ Entregable3Project/src/vista/Vista.java
@@ -8,16 +8,24 @@ import javax.swing.JPanel;
 
 public class Vista extends JFrame {
 
-	private JPanel panelPrincipal = new JPanel();
-	private CardLayout cardLayout = new CardLayout();
+	private JPanel panelPrincipal;
+	private CardLayout cardLayout;
 	
-	public void iniciar() {
+	public Vista() {
+		panelPrincipal = new JPanel();
+		cardLayout = new CardLayout();
+		panelPrincipal.setLayout(cardLayout);
+		
+		//Deberiamos guardarnos los IDs de cada sub-panel para hacer el show cuando corresponda.
+		
+		
+		
 		this.setTitle("Billetera Virtual");
 		frame.setSize(new Dimension(500,500));
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panelPrincipal);
-		frame.setVisible(true);
+		//frame.setVisible(true);  Esto deberia ir en el MAIN
 		
 		cardLayout.show(panelPrincipal, "MenuInicio");
 	}
